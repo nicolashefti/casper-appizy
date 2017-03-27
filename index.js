cfac32871f6ffd598378d41f4143a44f5c1f3aca
n.hefti <n.hefti@traveltrex.com>
2017-03-27 16:42:47 +0200

    Recursive page test start

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-var domain = casper.cli.get('domain') || 'http://www.google.com';
+/**
+ * Setup the root domain for the tests
+ */
+var domain = casper.cli.get('domain') || 'http://www.appizy.com';
 
-casper.test.begin('Hello, Test!', 1, function (test) {
+/**
+ * Setup a custom user agent to filter it from your normal visitors
+ */
+casper.userAgent('Mozilla/5.0 (MyCasperBot; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) Safari/538.1');
 
+casper.test.begin('Starting tests!', function (test) {
     casper.start(domain);
 
     casper.then(function () {
         casper.warn(casper.getCurrentUrl());
         test.assert(true);
+
+        casper.testPageInternUrl(3);
     });
 
-    casper.run(function(){
-       test.done();
+    casper.run(function () {
+        test.done();
     });
 });
+
+casper.testPageInternUrl = function (depth) {
+    var pageUrl = casper.getElementsAttribute('a', 'href');
+
+    var internUrl = pageUrl
+    // Filter intern
+        .filter(function (url) {
+            return !/^(http|#|\/\/)/.test(url) && url !== '' && url !== '/'
+        })
+        // Filter unique
+        .filter(function (element, position, self) {
+            return self.indexOf(element) == position;
+        });
+
+    casper.warn(JSON.stringify(internUrl));
+};
